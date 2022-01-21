69d159de632c944470926c39317fcbe5c3bb37ea
Hyeonae Jang <aeae_1@naver.com>
2022-01-21 22:38:54 +0900

    fix: AttendTodayResponseDto 값

--- server/server/src/main/java/jandi/server/model/AttendOneResponseDto.java
+++ server/server/src/main/java/jandi/server/model/AttendOneResponseDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.Comparator;
 
 @Getter @Setter
 public class AttendOneResponseDto implements Comparable<AttendOneResponseDto> {
--- server/server/src/main/java/jandi/server/model/AttendTodayResponseDto.java
+++ server/server/src/main/java/jandi/server/model/AttendTodayResponseDto.java
@@ -6,12 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AttendTodayResponseDto {
-    private String profileImage;
-    private String username;
+    private String name;
+    private String github;
     private boolean is_checked;
 
     public AttendTodayResponseDto(Attendance attendance) {
-        this.username = attendance.getUser().getName();
+        this.name = attendance.getUser().getName();
+        this.github = attendance.getUser().getGithub();
         this.is_checked = attendance.is_checked();
     }
 }
