e832e72f822ccb55242e01246570afb5469b9bb9
Hyeonae Jang <aeae_1@naver.com>
2022-01-26 02:16:02 +0900

    feat: Slack SDK for Java 연동

---
+++ server/server/src/main/java/jandi/server/util/ConversationsHistory.java
@@ -0,0 +1,52 @@
+package jandi.server.util;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.model.Message;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.PropertySource;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.emptyList;
+
+@PropertySource("classpath:app.properties")
+public class ConversationsHistory {
+
+    static String channelID = "";
+
+    static String token = "";
+
+
+    static Optional<List<Message>> conversationHistory = Optional.empty();
+
+    /**
+     * Fetch conversation history using ID from last example
+     */
+    static void fetchHistory(String id, String token) {
+        // you can get this instance via ctx.client() in a Bolt app
+        var client = Slack.getInstance().methods();
+        var logger = LoggerFactory.getLogger("my-awesome-slack-app");
+        try {
+            // Call the conversations.history method using the built-in WebClient
+            var result = client.conversationsHistory(r -> r
+                    // The token you used to initialize your app
+                    .token(token)
+                    .channel(id)
+            );
+            conversationHistory = Optional.ofNullable(result.getMessages());
+            // Print results
+            logger.info("{} messages found in {}", conversationHistory.orElse(emptyList()).size(), id);
+        } catch (IOException | SlackApiException e) {
+            logger.error("error: {}", e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Find conversation with a specified channel `name`
+        fetchHistory(channelID, token);
+    }
+
+}
